89cd65d9f3d625d9bb40eea862c4c4796ed03e1a
varadan13 <rcaesar1996@gmail.com>
2024-03-01 20:11:55 +0530

    add report menu error

--- createTemplate.mjs
+++ createTemplate.mjs
@@ -1,19 +1,30 @@
-import { CreateTemplateCommand } from "@aws-sdk/client-ses";
+import {
+  CreateTemplateCommand,
+  UpdateTemplateCommand,
+} from "@aws-sdk/client-ses";
 import client from "./client.mjs";
 
 import googleMerchantOptOut from "./templates/googleMerchantOptOut.mjs";
 import verceltemplate from "./templates/verceltemplate.mjs";
 import googleMerchantOptIn from "./templates/googleMerchantOptIn.mjs";
-
 import fancytemplate from "./templates/fancytemplate.mjs";
-const TEMPLATE_NAME = "GOOGLE_MERCHANT_OPT_IN";
-const SUBJECT = "You have a new order online opt in form submission!";
+
+import menuCorrects from "./templates/menuCorrects.mjs";
+
+const TEMPLATE_NAME = "REPORT_MENU_ERROR";
+const SUBJECT = "User reported menu or pricing issue";
+
+const fromIsAlways = ["noreply@kravein.com.au"];
+
+const to = ["restaurantEmail"]; // if restaurant email not found send to admin kravein
+const copyInProd = ["admin@kravein.com.au", "dave@bytelocal.com.au"];
+const copyInDev = ["noreply@kravein.com.au"];
 
 const createCreateTemplateCommand = () => {
-  return new CreateTemplateCommand({
+  return new UpdateTemplateCommand({
     Template: {
       TemplateName: TEMPLATE_NAME,
-      HtmlPart: googleMerchantOptIn,
+      HtmlPart: menuCorrects,
       SubjectPart: SUBJECT,
     },
   });
---
+++ templates/menuCorrects.mjs
@@ -0,0 +1,8 @@
+export default `
+<p>{{userName}} has reported the following issue in {{placename}} {{whichMenu}} </p>
+<p><span>Subject:&nbsp;</span><span>{{subject}}</span></p>
+<p>{{description}}</p>
+<p>Reported By</p>
+<p>{{userName}}</p>
+<p>{{userEmail}} {{userPhone}}</p>
+`;
